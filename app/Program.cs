99be5aa1f9b510bab3af223dd80fbc5c4864a067
Chris van der Pennen <chris.vanderpennen@gmail.com>
2022-04-22 10:49:43 +0930

    reproduce issue

---
+++ app/Program.cs
@@ -0,0 +1,2 @@
+﻿var obj = new GeneratedSchemas.Anonymous { Object = new GeneratedSchemas.Object { A = "A", C = "C", E = "E" }, String = "String" };
+Console.WriteLine(obj.ToJson());
---
+++ generator/Generator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using NJsonSchema;
+using NJsonSchema.CodeGeneration.CSharp;
+
+namespace generator 
+{
+    [Generator]
+    public class Generator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var settings = new CSharpGeneratorSettings
+            {
+                ClassStyle = CSharpClassStyle.Poco,
+                ArrayType = "System.Collections.Generic.List",
+                GenerateDataAnnotations = true,
+                NullHandling = NullHandling.Swagger,
+                RequiredPropertiesMustBeDefined = true,
+                DateTimeType = "System.DateTimeOffset",
+                
+                Namespace = "GeneratedSchemas",
+            };
+
+            foreach (var file in context.AdditionalFiles)
+            {
+                var name = Path.GetFileName(file.Path);
+                var schema = JsonSchema4.FromJsonAsync(file.GetText().ToString()).Result;
+                var generator = new CSharpGenerator(schema, settings);
+                context.AddSource($"{name}.cs", generator.GenerateFile(""));
+            }
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+    }
+}
